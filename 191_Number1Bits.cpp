25bc709cf24a490a1ba17c206d224466db1660ff
XTan-Smile <xiao.tan@duke.edu>
2015-06-15 14:58:20 -0400

    191_Number1Bits.cpp committed.

--- 191_Number1Bits.cpp
+++ 191_Number1Bits.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
   int hammingWeight(uint32_t n) {
+    /*** using & ***/
+    int counter = 0;
+    while (n) {
+        n = n & (n-1);
+        counter++;
+    }
+    return counter;
+    /*** using & end ***/
+    /*** recursion ***/
+    if (n == 0) return 0;
+    else return 1+hammingWeight(n & (n-1));
+    /*** recursion end ***/
+    /*** using sum ***/
+    uint32_t res = n;
+    int hamm = 0;
+    while(res >= 1) {
+      hamm += res%2;
+      res = res/2;
+    }
+    return hamm;
+    /*** using sum end ***/
     /*** practicing vector and template ***/
     uint32_t res = n;
     std::vector<uint32_t> ones;
@@ -19,15 +40,5 @@ public:
       return summ;
     }
     /*** practicing vector and template end ***/
-    /*** using an array ***/
-    uint32_t res = n;
-    int ones[32] = {0};
-    int hamm = 0;
-    while(res >= 1) {
-      hamm += res%2;
-      res = res/2;
-    }
-    return hamm;
-    /*** using an array end ***/
   }
 };
